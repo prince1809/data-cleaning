ef3c73564bc305b566e6e7652ac6eb3dd77587c8
prince <prince18pat@gmail.com>
2016-05-13 22:25:15 +0900

    Extracting and making codebook

--- run_analysis.R
+++ run_analysis.R
@@ -10,7 +10,7 @@
 #clean the workspace
 rm(list=ls())
 # Preparing the environment
-packages <- c("data.table","reshape2")
+packages <- c("data.table")
 sapply(packages, require, character.only=TRUE, quietly=TRUE)
 
 #Data Url
@@ -64,3 +64,22 @@ for (i in 1:6){
   extractedData$Activity[extractedData$Activity == i] <- as.character(activityLabels[i,2])
 }
 extractedData$Activity <- as.factor(extractedData$Activity)
+
+
+# 4. Appropriately labels the data set with descriptive variable names.
+names(extractedData) <- gsub("Acc","Accelerometer",names(extractedData))
+names(extractedData) <- gsub("Gyro","Gyroscope",names(extractedData))
+names(extractedData) <- gsub("BodyBody","Body",names(extractedData))
+names(extractedData) <- gsub("Mag","Magnitude",names(extractedData))
+names(extractedData) <- gsub("^t","Time",names(extractedData))
+names(extractedData) <- gsub("^f","frequency",names(extractedData))
+names(extractedData) <- gsub("-mean()","Mean",names(extractedData))
+names(extractedData) <- gsub("-std()","STD",names(extractedData))
+
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+extractedData$Subject <- as.factor(extractedData$Subject)
+extractedData <- data.table(extractedData)
+tidyData <- aggregate(. ~ Subject + Activity, extractedData, mean)
+tidyData <- tidyData[order(tidyData$Subject,tidyData$Activity),]
+write.table(tidyData, file = "Tidy.txt", row.names = FALSE)
